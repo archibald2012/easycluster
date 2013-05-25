4e64cec8dc5660cb66ce0a6d75a673f81fc65359
Archibald Wang <wq0032230@gmail.com>
2013-05-25 15:19:18 +0800

    add partitioned multi-threaded executor.

---
+++ easycluster-cluster/src/main/java/org/easycluster/easycluster/cluster/server/PartitionedThreadPoolMessageExecutor.java
@@ -0,0 +1,19 @@
+package org.easycluster.easycluster.cluster.server;
+
+import org.easycluster.easycluster.core.Closure;
+
+public class PartitionedThreadPoolMessageExecutor implements MessageExecutor {
+
+	@Override
+	public void execute(Object message, Closure closure) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void shutdown() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
